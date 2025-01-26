8cedd4516a4d8402a447a26ef0a5dbdd3ca14471
Frost <edwarddenicolas@gmail.com>
2025-01-26 11:54:05 +1100

    add init files

---
+++ src/exercises/MergeStringsAlternatively_1769.java
@@ -0,0 +1,28 @@
+package exercises;
+
+public class MergeStringsAlternatively_1769 {
+    public static void main(String[] args) {
+        String result = mergeAlternately("test1", "test2");
+        System.out.println(result);
+    }
+
+
+    private static String mergeAlternately(String word1, String word2) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i=0; i < word1.length() || i < word2.length(); i++) {
+
+            if (i < word1.length()) {
+                stringBuilder.append(word1.charAt(i));
+            }
+
+            if (i < word2.length()) {
+                stringBuilder.append(word2.charAt(i));
+            }
+
+        }
+
+        return stringBuilder.toString();
+    }
+
+}
