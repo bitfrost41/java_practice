2cd86f2abcd8924945e9fc8cb49567d08b16c953
Frost <edwarddenicolas@gmail.com>
2025-01-29 19:35:19 +1100

    reverse words

---
+++ src/exercises/ReverseWords_151.java
@@ -0,0 +1,34 @@
+package exercises;
+
+public class ReverseWords_151 {
+    public static void main(String[] args) {
+
+        String result = reverseWords(" hello world     test    ");
+        System.out.println(result);
+    }
+
+
+    private static String reverseWords(String s) {
+
+        // remove repeating, leading and trailing sapces
+        s = s.trim().replaceAll(" +", " ");
+
+        String[] sList = s.split(" ");
+
+        int i = 0, j = sList.length - 1;
+
+        while (i < j) {
+
+            String t = sList[i];
+            sList[i] = sList[j];
+            sList[j] = t;
+
+            ++i;
+            --j;
+        }
+
+        return String.join(" ", sList);
+    }
+
+
+}
