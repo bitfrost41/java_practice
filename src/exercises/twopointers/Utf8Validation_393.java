f908088aa54f2f94bab60175396fbbed4ed48720
Frost <edwarddenicolas@gmail.com>
2025-02-18 00:26:22 +1100

    new prob

---
+++ src/exercises/twopointers/Utf8Validation_393.java
@@ -0,0 +1,18 @@
+package exercises.twopointers;
+
+public class Utf8Validation_393 {
+    public static void main(String[] args) {
+
+        int[] data = { 197, 130, 1 };
+        boolean result = validUtf8(data);
+        System.out.println(result);
+    }
+
+    private static boolean validUtf8(int[] data) {
+
+        // solution
+
+        return false;
+    }
+
+}
