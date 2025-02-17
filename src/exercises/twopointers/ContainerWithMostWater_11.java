32c96782a426cff9a8a603e0abc9639c746dace1
Frost <edwarddenicolas@gmail.com>
2025-02-18 00:33:18 +1100

    fix

---
+++ src/exercises/twopointers/ContainerWithMostWater_11.java
@@ -0,0 +1,18 @@
+package exercises.twopointers;
+
+public class ContainerWithMostWater_11 {
+    public static void main(String[] args) {
+
+        int[] height = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+        int result = maxArea(height);
+        System.out.println(result);
+    }
+
+    private static int maxArea(int[] height) {
+
+        // solution
+
+        return 0;
+    }
+
+}
