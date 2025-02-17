b459bb80db8b9341c1873ca490486a26ecabe59d
Frost <edwarddenicolas@gmail.com>
2025-02-17 23:44:59 +1100

    temp

---
+++ src/exercises/twopointers/IsSubsequence_392.java
@@ -0,0 +1,19 @@
+package exercises.twopointers;
+
+public class IsSubsequence_392 {
+    public static void main(String[] args) {
+
+        boolean result = isSubsequence("abc", "ahbgdc");
+        System.out.println(result);
+    }
+
+    private static boolean isSubsequence(String s, String t) {
+
+        // solution
+
+        
+
+        return false;
+    }
+
+}
