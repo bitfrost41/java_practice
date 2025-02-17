2725aa748b7ea12df9ce9391a631f690ded32e62
Frost <edwarddenicolas@gmail.com>
2025-02-18 00:23:44 +1100

    prob

--- src/exercises/twopointers/IsSubsequence_392.java
+++ src/exercises/twopointers/IsSubsequence_392.java
@@ -3,15 +3,42 @@ package exercises.twopointers;
 public class IsSubsequence_392 {
     public static void main(String[] args) {
 
-        boolean result = isSubsequence("abc", "ahbgdc");
+        boolean result = isSubsequence("bd", "ahbgdc");
         System.out.println(result);
     }
 
     private static boolean isSubsequence(String s, String t) {
 
-        // solution
+        // blank defaults to true
+        if (s.length() == 0) {
+            return true;
+        }
 
-        
+        int sIndex = 0;
+        int tIndex = 0;
+
+        char[] sChar = s.toCharArray();
+        char[] tChar = t.toCharArray();
+
+        int N = tChar.length;
+
+        // loop until t runs out
+        while (tIndex < N) {
+
+            // evaluate sChar
+            if (sChar[sIndex] == tChar[tIndex]) {
+                // if equal, s char is in t string. then move s index
+                sIndex++;
+
+                // if it reached the end of s before terminating in t, then subsequence is found
+                if (sIndex == sChar.length) {
+                    return true;
+                }
+            }
+
+            tIndex++;
+
+        }
 
         return false;
     }
