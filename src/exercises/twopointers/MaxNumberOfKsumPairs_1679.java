3e9875a458b62fcff89e6e6c4a4380352154b34b
Frost <edwarddenicolas@gmail.com>
2025-02-24 23:54:22 +1100

    add next prob

---
+++ src/exercises/twopointers/MaxNumberOfKsumPairs_1679.java
@@ -0,0 +1,18 @@
+package exercises.twopointers;
+
+public class MaxNumberOfKsumPairs_1679 {
+    public static void main(String[] args) {
+
+        int[] nums = { 3, 1, 3, 4, 3 };
+        int result = maxOperations(nums, 5);
+        System.out.println(result);
+    }
+
+    private static int maxOperations(int[] nums, int k) {
+
+        // solution
+
+        return 0;
+    }
+
+}
