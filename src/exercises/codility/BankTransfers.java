d577551cdba141ce3e4e6cf19a1079488657c71e
Frost <edwarddenicolas@gmail.com>
2025-03-19 21:02:26 +1100

    add problems

---
+++ src/exercises/codility/BankTransfers.java
@@ -0,0 +1,16 @@
+package exercises.codility;
+
+public class BankTransfers {
+    public static void main(String[] args) {
+
+        int[] P = { 4, 4, 2, 4 };
+
+        int result = solution(P);
+        System.out.println(result);
+    }
+
+    private static int solution(int[] P) {
+        return 0;
+    }
+
+}
---
+++ src/exercises/codility/NumberOfPotholes.java
@@ -0,0 +1,16 @@
+package exercises.codility;
+
+public class NumberOfPotholes {
+    public static void main(String[] args) {
+
+        int[] P = { 4, 4, 2, 4 };
+
+        int result = solution(P);
+        System.out.println(result);
+    }
+
+    private static int solution(int[] P) {
+        return 0;
+    }
+
+}
