f7d0147540b1085745584694e78619fd5b8fccee
Frost <edwarddenicolas@gmail.com>
2025-03-20 00:24:55 +1100

    prob

--- src/exercises/codility/PlayerMovement.java
+++ src/exercises/codility/PlayerMovement.java
@@ -1,7 +1,13 @@
 package exercises.codility;
 
-import java.util.HashSet;
-import java.util.Set;
+/*
+There are N players standing in a row, one player on a field. They are numbered from 0 to N-1 from left to right.
+Players perform moves one by one from left to right, that is, in ascending order of numbers. Each player presses an arrow key in one of the four cardinal directions: left ('<), right (>), up ('*) or down (v). A key press in the given direction means that the player attempts to move onto the closest field in the direction specified. A move can be performed only if there is no other player already standing on the target field.
+Moves are represented as a string S of length N, where S[K] (for K within the range O..N-1) is the direction of the K-th player's move. How many players will actually perform a move successfully?
+Write a function:
+class Solution f public int solution (String S); }
+which, given a string S of length N representing arrow keys pressed by each of the players, returns the number of players that will perform a move successfully.
+*/
 
 public class PlayerMovement {
     public static void main(String[] args) {
