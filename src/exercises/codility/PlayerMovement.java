0bf2d88f37a501edf2c34672b489f86e740ddbe3
Frost <edwarddenicolas@gmail.com>
2025-03-20 00:24:13 +1100

    player movement prob

--- src/exercises/codility/PlayerMovement.java
+++ src/exercises/codility/PlayerMovement.java
@@ -1,16 +1,57 @@
 package exercises.codility;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class PlayerMovement {
     public static void main(String[] args) {
 
-        int[] P = { 4, 4, 2, 4 };
+        String S = "^>VV<>";
 
-        int result = solution(P);
+        int result = solution(S);
         System.out.println(result);
     }
 
-    private static int solution(int[] P) {
-        return 0;
+    private static int solution(String S) {
+        int N = S.length();
+        char[] cs = S.toCharArray();
+
+        int[] players = new int[N];
+        int successfulMoves = 0;
+
+        // Populate array with players
+        for (int i = 0; i < N; i++) {
+            players[i] = 1;
+        }
+
+        boolean isMoveSuccessful = false;
+        for (int i = 0; i < N; i++) {
+            if (cs[i] == '<') {
+                if (i == 0) {
+                    isMoveSuccessful = true; // automatic success on first element
+                } else {
+                    isMoveSuccessful = players[i - 1] == 0; // check if player left the place
+                }
+            } else if (cs[i] == '>') {
+                if (i == N - 1) {
+                    isMoveSuccessful = true; // automatic success on last element
+                } else {
+                    isMoveSuccessful = false; // automatic fail as there will always be a player on the right
+                }
+            } else if (cs[i] == '^') {
+                isMoveSuccessful = true; // automatic success on move up
+            } else if (cs[i] == 'V') {
+                isMoveSuccessful = true; // automatic success on move down
+            }
+
+            // Add to successful move
+            if (isMoveSuccessful) {
+                successfulMoves++;
+                players[i] = 0;
+            }
+        }
+
+        return successfulMoves;
     }
 
 }
